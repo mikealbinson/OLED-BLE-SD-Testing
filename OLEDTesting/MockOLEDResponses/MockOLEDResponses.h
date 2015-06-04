37f84f579bd289b9234c3d863dbb596113001efa
mikealbinson <michael_albinson@ticketfly.com>
2015-06-04 10:26:57 -0700

    Create MockOLEDResponses.h

---
+++ OLEDTesting/MockOLEDResponses/MockOLEDResponses.h
@@ -0,0 +1,26 @@
+#ifndef _MOCKOLEDRESPONSES_H
+#define _MOCKOLEDRESPONSES_H
+
+#include <U8gLibClone.h>
+#include <Testing.h>
+#include "Arduino.h"
+
+int printToOLED (String printThis);
+
+class MockOLED{
+	private:
+	U8GLIB_SSD1306_128X32MOCK *MOCKu8g;
+		
+	
+  public:
+		int MockPrintToOLED (String printThis);
+		int setColorIndexMock(int type);
+  	MockOLED();
+		void drawSetupMock(MockOLED &obj);
+  	const char* drawGeneralMock(String stringToPrint, MockOLED &obj);
+};
+
+
+
+#endif // MockOLEDResponses.h
+
