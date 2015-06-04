811fa4fab7b8f5a683c3f48bd32be19a7fa9e09d
mikealbinson <michael_albinson@ticketfly.com>
2015-06-04 10:27:14 -0700

    Create MockOLEDResponses.cpp

---
+++ OLEDTesting/MockOLEDResponses/MockOLEDResponses.cpp
@@ -0,0 +1,36 @@
+#include "MockOLEDResponses.h"
+
+int MockOLED::MockPrintToOLED (String printThis){
+	return 1; //if true... which I guess is true every time
+}
+
+int MockOLED::setColorIndexMock (int type){
+	int happy = type;
+	return 1; //if done
+}
+
+MockOLED::MockOLED () {
+	MOCKu8g = new U8GLIB_SSD1306_128X32MOCK(U8G_I2C_OPT_NONE);
+  MOCKu8g->getMode() == U8G_MODE_BW;
+  MOCKu8g->setColorIndex(1);
+  TestTrue ("OLED init test", MOCKu8g);
+}
+
+void MockOLED::drawSetupMock(MockOLED& obj) {
+	//const char* final;
+	int printed = obj.MockPrintToOLED ("Setting Up"); //There's an interior method here that interacts with the OLED
+	TestEqual ("drawSetup Print Check", printed, 1 );
+}
+
+const char* MockOLED::drawGeneralMock(String stringToPrint,MockOLED& obj) {
+	const char* conversion;
+	const char* smaller;
+  conversion = stringToPrint.c_str();
+	int printed = obj.MockPrintToOLED (conversion); //There's an interior method here that interacts with the OLED
+	TestEqual ("drawGeneralPrintCheck", printed, 1 );
+	return conversion;
+}
+
+//Still hoping to include a bitmap fxn at some point if I can figure out how to get a bitmap in the proper format
+
+//Done Shortening --Start Testing tomorrow
