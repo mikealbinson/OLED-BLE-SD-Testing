b97c4f6427a066cc304694b36fb623ecccd554da
mikealbinson <michael_albinson@ticketfly.com>
2015-06-03 17:46:39 -0700

    Update Testing.h

--- Testing/Testing.h
+++ Testing/Testing.h
@@ -3,8 +3,8 @@
 
 #include "Arduino.h"
 
-void success();
-void fail();
+void success(String testName);
+void fail(String testName, int testType);
 const char* StringtoArray(String changeToCArray);
 
 void TestEqual (String testName, String one, String two);
@@ -13,11 +13,18 @@ void TestEqual (String testName, String one, const char* two);
 void TestEqual (String testName, int one, int two);
 void TestEqual (String testName, char one, char two);
 void TestEqual (String testName, byte one, byte two);
+void TestEqual (String testName, bool one, bool two);
 
 void TestNotEqual (String testName, String one, String two);
 void TestNotEqual (String testName, const char* one, const char* two);
 void TestNotEqual (String testName, int one, int two);
 void TestNotEqual (String testName, char one, char two);
 void TestNotEqual (String testName, byte one, byte two);
+
+void TestEmpty (String testName, int arg);
+void TestEmpty (String testName, const char* arg);
+void TestEmpty (String testName, String arg);
+void TestEmpty (String testName, char arg);
+void TestEmpty (String testName, byte arg);
 
 #endif
