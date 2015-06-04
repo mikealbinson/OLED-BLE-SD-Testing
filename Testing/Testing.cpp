4edc77532e4259b6402997b95de7e2ae4890b1a5
mikealbinson <michael_albinson@ticketfly.com>
2015-06-03 17:47:04 -0700

    Update Testing.cpp

--- Testing/Testing.cpp
+++ Testing/Testing.cpp
@@ -5,9 +5,32 @@ void success (String testName){
   Serial.println (F(" was a success"));
 }
 
-void fail ( String testName) {
+void fail ( String testName, int testType) {
 	Serial.print (testName);
-  Serial.println (F(" failed. Review funciton."));
+  Serial.print (F(" failed the ") );
+  switch (testType){
+	
+		case 1:
+		Serial.println (F("equality"));
+		break;
+		
+		case 2:
+		Serial.println (F("inequality"));
+		break;
+		
+		case 3:
+		Serial.println (F("empty variable"));
+		break;
+		
+		case 4:
+		Serial.println (F("boolean"));
+		break;
+		
+		default:
+		Serial.println (F("???"));
+		break;
+		Serial.println (F(" test. Review funciton."));
+	};
 }
 
 const char* StringtoArray(String changeToCArray){
@@ -22,7 +45,7 @@ void TestEqual (String testName, String one, String two){
 	if (convertedOne == convertedTwo)
     success (testName);
   else
-    fail (testName);
+    fail (testName,1);
 }
 
 void TestEqual (String testName, const char* one, const char* two){
@@ -30,7 +53,7 @@ void TestEqual (String testName, const char* one, const char* two){
   if (one == two) 
     success (testName);
   else
-    fail (testName);
+    fail (testName,1);
 }
 
 void TestEqual (String testName, String one, const char* two){
@@ -39,7 +62,7 @@ void TestEqual (String testName, String one, const char* two){
 	if (converted == two) 
     success (testName);
   else
-    fail (testName);
+    fail (testName,1);
 }
 
 void TestEqual (String testName, const char* one, String two){
@@ -48,28 +71,35 @@ void TestEqual (String testName, const char* one, String two){
 	if (one == converted) 
     success (testName);
   else
-    fail (testName);
+    fail (testName,1);
 }
 
 void TestEqual (String testName, int one, int two){
   if (one == two) 
     success (testName);
   else
-    fail (testName);
+    fail (testName,1);
 }
 
 void TestEqual (String testName, char one, char two){
   if (one == two) 
     success (testName);
   else
-    fail (testName);
+    fail (testName,1);
 }
 
 void TestEqual (String testName, byte one, byte two){
   if (one == two) 
     success (testName);
   else
-    fail (testName);
+    fail (testName,1);
+}
+
+void TestEqual (String testName, bool one, bool two){
+	if (one == two) 
+    success (testName);
+  else
+    fail (testName,1);
 }
 
 void TestNotEqual (String testName, String one, String two){
@@ -78,33 +108,77 @@ void TestNotEqual (String testName, String one, String two){
   if (convertedOne != convertedTwo) 
     success (testName);
   else
-    fail (testName);
+    fail (testName,2);
 }
 
 void TestNotEqual (String testName, const char* one, const char* two){
   if (one != two) 
     success (testName);
   else
-    fail (testName);
+		fail (testName,2);	
 }
 
 void TestNotEqual (String testName, int one, int two){
   if (one != two) 
     success (testName);
   else
-    fail (testName);
+		fail (testName,2);
 }
 
 void TestNotEqual (String testName, char one, char two){
   if (one != two) 
     success (testName);
   else
-    fail (testName);
+    fail (testName,2);
 }
 
 void TestNotEqual (String testName, byte one, byte two){
   if (one != two) 
     success (testName);
   else
-    fail (testName);
+    fail (testName,2);
 }
+
+void TestEmpty (String testName, int arg){
+	if (arg == 0) 
+    success (testName);
+  else
+    fail (testName,3);
+}
+
+void TestEmpty (String testName, const char * arg){
+	if (arg == 0) 
+    success (testName);
+  else
+    fail (testName,3);
+}
+
+void TestEmpty (String testName, String arg){
+	if (arg == "") 
+    success (testName);
+  else
+    fail (testName,3);
+}
+
+void TestEmpty (String testName, char arg){
+	if (arg == 0) 
+    success (testName);
+  else
+    fail (testName,3);
+}
+
+void TestEmpty (String testName, byte arg){
+	if (arg == 0) 
+    success (testName);
+  else
+    fail (testName,3);
+}
+
+void TestTrue (String testName, bool arg){
+	if (arg)
+		success (testName);
+  else
+    fail (testName,4);
+}
+
+
