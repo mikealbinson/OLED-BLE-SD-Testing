5974d453a312031ce3908ae321c87f9c4914677c
mikealbinson <michael_albinson@ticketfly.com>
2015-06-05 11:45:47 -0700

    Create mocks.h

---
+++ mocks/mocks.h
@@ -0,0 +1,80 @@
+#ifndef MOCKS_H_
+#define MOCKS_H_
+
+#include "arduino.h"
+#include <U8gLibClone.h>
+#include <Testing.h>
+#include <SPI.h>
+#include <boards.h>
+#include <RBL_LibCLONE.h>
+
+#define CHARS_TO_READ 14
+
+void testingScenario ();
+void endOfScenario ();
+
+class MockFile{
+public:
+	MockFile();
+	bool read();
+	bool read (char* interest, int that);
+	bool close();
+	String println(String lineToPrint);
+	bool available();
+	
+private:
+	String readerString = "Hello There";
+};
+
+class MockSdFat {
+ public:
+	MockSdFat();
+	bool open(String file);
+	bool open(String file, const uint8_t function);
+	bool begin(int i);
+	bool remove (String file);
+	bool available(); 
+
+};
+
+class MockSDBeacon{
+public:
+	  MockSDBeacon();
+		void SDBeaconSetup (MockSdFat& SD);
+		bool searchDatabaseForTicket(String TicketString, MockSdFat& SD,  MockFile& myFile);
+		void writeToSD (String writeThisString, String fileName, MockSdFat& SD, MockFile& myFile);
+		void simpleReadSD (String fileName, MockSdFat& SD, MockFile& myFile);
+		void fileRemove (String fileName, MockSdFat& SD, MockFile& myFile);
+};
+
+class MockOLED{
+	private:
+	U8GLIB_SSD1306_128X32MOCK *MOCKu8g;
+	bool exists;
+		
+	
+  public:
+		int MockPrintToOLED (String printThis);
+		int setColorIndexMock(int type);
+  	MockOLED();
+		void drawSetupMock(MockOLED &obj);
+  	const char* drawGeneralMock(String stringToPrint, MockOLED &obj);
+};
+
+class MockBLE {
+public:
+	MockBLE();
+	void TflyBLESetup ();
+	void clearParsingSetups(String stringToClear, char charArrayToClear[]);
+	void beaconSendData();
+	String readIncoming();
+	
+private:
+	int ble_available = true; //let's assume we're costantly connected
+	char MockBLE_Read(int &i);
+	const char* MockBLE_write (int carrier, int &i);
+	
+};
+
+
+#endif
