dfe1d3c3c70431b79d5f91b571e4953ddb7f0d57
mikealbinson <michael_albinson@ticketfly.com>
2015-06-05 11:47:28 -0700

    Create MockTesting.ino

---
+++ mocks/examples/MockTesting.ino
@@ -0,0 +1,29 @@
+#include <U8gLibClone.h>
+#include <Testing.h>
+#include <SPI.h>
+#include <boards.h>
+#include <RBL_LibCLONE.h>
+#include <mocks.h>
+
+MockOLED oled1;
+MockFile myFile;
+MockSdFat Sdfat;
+MockSDBeacon SDB;
+MockBLE ble;
+String stringInterface = "";
+
+void setup() {
+  testingScenario();
+  ble.TflyBLESetup();
+  oled1.drawSetupMock(oled1);
+  SDB.SDBeaconSetup (Sdfat);
+  stringInterface = ble.readIncoming();
+  TestTrue ("The ticket is found on the SD", SDB.searchDatabaseForTicket (stringInterface, Sdfat, myFile));
+  oled1.drawGeneralMock ("TicketTypeDisplayed", oled1);
+  endOfScenario ();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
